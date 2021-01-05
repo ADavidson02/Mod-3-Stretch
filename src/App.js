11fd6402aecaa7c9813722e67465f3618d7a9ee0
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-05 16:53:39 -0700

    Initial commit and add router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <Switch>
+          <Route
+            path="/"
+            render={() => {
+              return "Home";
+            }}
+          />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
