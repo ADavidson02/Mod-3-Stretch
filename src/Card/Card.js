8804aefd83d5197927c009fd50239218292dba96
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-05 17:41:08 -0700

    Add card functional component

---
+++ src/Card/Card.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const Card = () => {
+  return(
+    <section>
+      <article>
+        Advice
+      </article>
+      <button>Delete</button>
+    </section>
+  )
+}
+
+export default Card;
