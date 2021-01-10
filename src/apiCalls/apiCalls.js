6a7a14c145285577c335e49834a93612688728d1
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-09 20:06:50 -0700

    Refactor data recieved from api call

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -4,7 +4,7 @@ export const getNewRandom = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
   if (response.status >= 200 && response.status <= 299 ) {
     const jsonResponse = await response.json();
-    return jsonResponse
+    return jsonResponse.slip
   } else {
     console.log(`Error ${response.status}`)
     return response.status;
