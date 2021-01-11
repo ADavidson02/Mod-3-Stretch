8ab9160bdae493123ef43a9c47f6172cf936e292
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-11 14:04:21 -0700

    Add test to Card component to check for button display

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -7,18 +7,40 @@ import Card from "./Card"
 describe("Card", () => {
   it("should render correctly", () => {
     const mockAdviceObj = {
-      id: 22, advice: "Go to sleep early"
+      id: 22,
+      advice: "Go to sleep early",
+      email: 'abc@123.com',
+      name: 'Mike'
     }
     render(
-      <Card 
-      slip={mockAdviceObj}
+      <Card
+        id={mockAdviceObj.id}
+        advice={mockAdviceObj.advice}
+        email={mockAdviceObj.email}
+        name={mockAdviceObj.name}
       />
-    )
+    );
 
     const deleteButton = screen.getByText("Delete")
     expect(screen.getByText("Go to sleep early")).toBeInTheDocument();
     expect(deleteButton).toBeInTheDocument();
-  })
+  }),
+
+    it("should render without a delete button when anonymous", () => {
+    const mockAdviceObj = {
+      id: 22,
+      advice: "Go to sleep late"
+    }
+    render(
+      <Card
+        id={mockAdviceObj.id}
+        advice={mockAdviceObj.advice}
+      />
+    );
+    const deleteButton = screen.queryByText("Delete")
+    expect(deleteButton).not.toBeInTheDocument();
+    expect(screen.getByText("Go to sleep late")).toBeInTheDocument();
+    })
 
   //Need to add test once delete button is functional 
 })
