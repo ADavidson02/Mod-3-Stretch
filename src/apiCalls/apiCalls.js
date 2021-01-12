0517dba34a12646da7205f4d7d083612ce0454f6
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-12 16:23:55 -0700

    Clean up and alter name of call

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,18 +1,18 @@
 import axios from "axios"
 
-export const getNewRandom = async () => {
+export const getRandomAdvice = async () => {
   const response = await fetch("https://api.adviceslip.com/advice");
-  if (response.status >= 200 && response.status <= 299 ) {
+  if (response.status >= 200 && response.status <= 299) {
     const jsonResponse = await response.json();
-    return jsonResponse.slip
+    return jsonResponse.slip;
   } else {
-    console.log(`Error ${response.status}`)
+    console.log(`Error ${response.status}`);
     return response.status;
   }
-}
+};
 
 export const createAdvice = async (advice, email, name, id) => {
-  axios.post('https://us-central1-advizer-development.cloudfunctions.net/user', {
+  axios.post("https://us-central1-advizer-development.cloudfunctions.net/user", {
     advice: advice,
     email: email,
     name: name,
