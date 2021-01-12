b2e26ed16fbfe9a55001bb0345a96336f8d7fa4d
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-11 19:50:17 -0700

    Clean up Home test

--- src/Home/Home.test.js
+++ src/Home/Home.test.js
@@ -1,51 +1,41 @@
+
 import { screen, render, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
-import Home from "./Home";
 import Card from "../Card/Card"
-import { MemoryRouter, Router } from "react-router-dom";
-import App from "../App/App";
-import { getNewRandom } from "../apiCalls/apiCalls";
 jest.mock("../apiCalls/apiCalls");
 
 describe("Home", () => {
-
   it('should render two advice buttons', async () => {
-
     const getNewRandom1 = { 
       id: 45,
       advice: "Do not kick children"
     };
-
-        render(
-          <section className="home">
-            <div>
-              <article>
-                <Card slip={getNewRandom1} />
-                <h2></h2>
-              </article>
-            </div>
-            <div>
-              <button onClick={() => this.getNewRandom()}>
-                Get New Advice
-              </button>
-              <button>Add New Advice</button>
-            </div>
-          </section>
-        );
+    render(
+      <section className="home">
+        <div>
+          <article>
+            <Card slip={getNewRandom1} />
+            <h2></h2>
+          </article>
+        </div>
+        <div>
+          <button onClick={() => this.getNewRandom()}>
+            Get New Advice
+          </button>
+          <button>Save Advice</button>
+        </div>
+      </section>
+    );
     const newRandomButton = screen.getByText("Get New Advice");
-    const addNewButton = screen.getByText("Add New Advice");
+    const addNewButton = screen.getByText("Save Advice");
     expect(newRandomButton).toBeInTheDocument();
     expect(addNewButton).toBeInTheDocument();
-    
   }),
   
   it('should render an advice card', async () => {
-    
     const getNewRandom = (
     { id: 42, advice: "Do not eat yellow snow" }
     );
-
     render(
       <section className="home">
         <div>
@@ -56,30 +46,13 @@ describe("Home", () => {
         </div>
         <div>
           <button onClick={() => this.getNewRandom()}>Get New Advice</button>
-          <button>Add New Advice</button>
+          <button>Save Advice</button>
         </div>
       </section>
     );
-
     const adviceSlip = await waitFor(() =>
       screen.getByText("Do not eat yellow snow")
     );
     expect(screen.getByText("Do not eat yellow snow")).toBeInTheDocument()
   })
-
-  // it.only("should route back to archive upon adding advice", async () => {
-  //   render(
-  //     <MemoryRouter>
-  //       <App />
-  //     </MemoryRouter>
-  //   )
-
-  //   const archiveLink = await waitFor(() => {
-  //     screen.getByText("Add New Advice")
-  //   })
-  //   userEvent.click(archiveLink)
-  //   expect(screen.queryByText("Get New Advice")).not.toBeInTheDocument();
-  //   screen.debug
-  // })
-
 })
