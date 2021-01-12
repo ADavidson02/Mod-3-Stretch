bb0c1b21365d3b28602a0ef07e9f65f7518529e1
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-11 19:45:00 -0700

    Complete testing Form component

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -3,27 +3,30 @@ import { screen, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Form from "../Form/Form";
+import { createAdvice } from "../apiCalls/apiCalls"
+jest.mock("../apiCalls/apiCalls");
+import { createMemoryHistory } from "history";
 
 
 describe("Form", () => {
-  it("should have three input fields", () => {
+  it("should render the form correctly", () => {
+
     render(
       <Form />
     )
     expect(screen.getByPlaceholderText("Add Advice Here")).toBeInTheDocument()
     expect(screen.getByPlaceholderText("Name")).toBeInTheDocument()
     expect(screen.getByPlaceholderText("Email")).toBeInTheDocument()
+    expect(screen.getByText("Save Advice")).toBeInTheDocument()
   }),
 
   it("should reflect what user types in inputs", () => {
     render(
       <Form />
     )
-
     userEvent.type(screen.getByPlaceholderText("Add Advice Here"), "Do not speed")
     userEvent.type(screen.getByPlaceholderText("Name"), "Amy")
     userEvent.type(screen.getByPlaceholderText("Email"), "hope@745.com")
-
 
     expect(screen.getByPlaceholderText("Add Advice Here").value).toEqual("Do not speed")
     expect(screen.getByPlaceholderText("Name").value).toEqual("Amy")
@@ -39,25 +42,5 @@ describe("Form", () => {
     userEvent.type(screen.getByPlaceholderText("Email"), "hope@745.com");
     userEvent.click(submitButton)
     expect(screen.getByText("Please fill out all fields")).toBeInTheDocument()
-
   })
-
-  // it("should clear inputs upon submission", () => {
-  //   render(
-  //     <Form />
-  //   )
-  //   createAdvice.mockResolvedValue()
-  //   const submitButton = screen.getByText("Save Advice")
-  //   userEvent.type(screen.getByPlaceholderText("Add Advice Here"), "Do not speed");
-  //   userEvent.type(screen.getByPlaceholderText("Name"), "Amy");
-  //   userEvent.type(screen.getByPlaceholderText("Email"), "hope@745.com");
-
-  //   userEvent.click(submitButton)
-
-
-  //   expect(screen.getByPlaceholderText("Add Advice Here").value).toEqual("Add Advice Here");
-  //   expect(screen.getByPlaceholderText("Name").value).toEqual("Name");
-  //   expect(screen.getByPlaceholderText("Email").value).toEqual("Email");
-  //   screen.debug
-  // })
 })
