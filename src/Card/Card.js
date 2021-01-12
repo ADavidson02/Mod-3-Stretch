604d65cbc5559d86a1b97059d41801e6f155a0da
Estelle Staffieri <estellelareinne@gmail.com>
2021-01-11 23:51:26 -0700

    Add styling to archive component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,16 +1,17 @@
+import "./Card.css";
 import React from "react";
 
 const Card = (props) => {
   // console.log(props.id, props.advice)
   // console.log(props)
   return (
-    <section>
+    <section className="advice-card">
       <article key={props.id} className="text-center mb-4">
         <h2>{props.advice || props.slip.advice}</h2>
         <p>{props.email}</p>
         <p>{props.name}</p>
       </article>
-      {props.name && <button onClick={() => props.deleteAdviceCard(props)}>Delete</button>}
+      {props.name && <button className="delete-button" onClick={() => props.deleteAdviceCard(props)}>Delete</button>}
     </section>
   );
 };
