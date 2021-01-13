0935a925fce3994232dd07dc336cf51c88f010f7
Amanda Davidson <AmandaADavidson@hotmail.com>
2021-01-12 17:13:06 -0700

    Remove unfunctioning components

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,39 +1,39 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth } from "../firebase"
+// import React, { useContext, useState, useEffect } from 'react'
+// import { auth } from "../firebase"
 
-const AuthContext = React.createContext()
+// const AuthContext = React.createContext()
 
-export function useAuth() {
-  return useContext(AuthContext)
-}
+// export function useAuth() {
+//   return useContext(AuthContext)
+// }
 
-export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState()
+// export function AuthProvider({ children }) {
+//   const [currentUser, setCurrentUser] = useState()
 
-  function signup(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password)
-  }
+//   function signup(email, password) {
+//     return auth.createUserWithEmailAndPassword(email, password)
+//   }
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      setCurrentUser(user)
-    })
-    return unsubscribe
-  }, [])
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged(user => {
+//       setCurrentUser(user)
+//     })
+//     return unsubscribe
+//   }, [])
 
-  function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
-  }
+//   function login(email, password) {
+//     return auth.signInWithEmailAndPassword(email, password)
+//   }
 
-  const value = {
-    currentUser,
-    signup,
-    login
-  }
+//   const value = {
+//     currentUser,
+//     signup,
+//     login
+//   }
 
-  return (
-    <AuthContext.Provider value={ value }>
-      { children }
-    </AuthContext.Provider>
-  )
-}
+//   return (
+//     <AuthContext.Provider value={ value }>
+//       { children }
+//     </AuthContext.Provider>
+//   )
+// }
